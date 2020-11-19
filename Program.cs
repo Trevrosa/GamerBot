f9f058561bfa8fd1c0af201b905a19454db1e029
Trevrosa <49025203+Trevrosa@users.noreply.github.com>
2020-11-19 17:48:36 +0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Interactivity.Extensions;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+// ReSharper disable HeapView.ObjectAllocation
+
+// ReSharper disable HeapView.DelegateAllocation
+// ReSharper disable HeapView.ObjectAllocation.Evident
+// ReSharper disable HeapView.ClosureAllocation
+// ReSharper disable HeapView.ObjectAllocation.Possible
+// ReSharper disable HeapView.BoxingAllocation
+
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable PossibleMultipleEnumeration
+// ReSharper disable MemberCanBeMadeStatic.Local
+// ReSharper disable ConvertToConstant.Local
+
+// ReSharper disable InvertIf
+// ReSharper disable ArrangeThisQualifier
+// ReSharper disable SuggestVarOrType_BuiltInTypes
+// ReSharper disable UnusedMember.Global
+// ReSharper disable RedundantAssignment
+// ReSharper disable RedundantStringInterpolation
+// ReSharper disable RedundantArgumentDefaultValue
+// ReSharper disable UnusedAutoPropertyAccessor.Local
+// ReSharper disable SuggestVarOrType_SimpleTypes
+// ReSharper disable RedundantToStringCall
+
+namespace GamerBot
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            new Bot().RunAsync().GetAwaiter().GetResult();
+        }
+
+        public class Bot
+        {
+            public DiscordClient Client { get; private set; }
+            public InteractivityExtension Interactivity { get; private set; }
+            public CommandsNextExtension Commands { get; private set; }
+
+            public async Task RunAsync()
+            {
+                var json = string.Empty;
+
+                await using (var fs = File.OpenRead("config.json"))
+                using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                    json = await sr.ReadToEndAsync().ConfigureAwait(false);
+
+                var cfgjson = JsonConvert.DeserializeObject<ConfigJson>(json);
+
+                var config = new DiscordConfiguration
+                {
+                    Token = cfgjson.Token,
+                    TokenType = TokenType.Bot,
+                    AutoReconnect = true,
+                    MinimumLogLevel = LogLevel.Debug,
+                    Intents = DiscordIntents.AllUnprivileged | DiscordIntents.GuildMembers
+                };
+                this.Client = new DiscordClient(config);
+
+                this.Client.Ready += this.Client_Ready;
+
+                Client.UseInteractivity(new InteractivityConfiguration
+                {
+                    Timeout = TimeSpan.FromMinutes(10)
+                });
+
+                var commandsConfig = new CommandsNextConfiguration
+                {
+                    StringPrefixes = new[] { cfgjson.CommandPrefix },
+                    EnableDms = true,
+                    EnableMentionPrefix = true,
+                    CaseSensitive = false,
+                    DmHelp = false
+                };
+
+                Commands = Client.UseCommandsNext(commandsConfig);
+
+                Commands.RegisterCommands<Youtube>();
+                
+                AppDomain.CurrentDomain.ProcessExit += async (s, ev) =>
+                {
+                    await Client.DisconnectAsync();
+                };
+
+                Console.CancelKeyPress += async (s, ev) =>
+                {
+                    await Client.DisconnectAsync();
+                };
+
+                await Client.ConnectAsync();
+
+                await Task.Delay(-1);
+            }
+
+            private async Task Client_Ready(DiscordClient client, ReadyEventArgs e)
+            {
+                await Client.UpdateStatusAsync(new DiscordActivity($"over N/A shards.", ActivityType.Watching), UserStatus.Online);
+            }
+        }
+
+        public struct ConfigJson
+        {
+            [JsonProperty("token")]
+            public string Token { get; private set; }
+
+            [JsonProperty("prefix")]
+            public string CommandPrefix { get; private set; }
+        }
+    }
+}
---
+++ Youtube.cs
@@ -0,0 +1,20 @@
+﻿// ReSharper disable RedundantUsingDirective
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedParameter.Global
+
+namespace GamerBot
+{
+    [Group("youtube")]
+    internal class Youtube : BaseCommandModule
+    {
+        [Command("download"), Description("Downloads a youtube video and sends the file back. (MP3 and MP4)")]
+        public async Task Download(CommandContext ctx, [Description("Link to the video you want to download.")] string link = null, [Description("Choose what type of file you want to download.")] string type = "mp4")
+        {
+            await ctx.RespondAsync("");
+        }
+    }
+}
